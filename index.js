bc77732bf3c36df53fec1ef2cd26729cea3df49a
Dylan <dylan.vonwald@gmail.com>
2022-06-16 18:25:42 -0600

    pseudocode and boilerplate and tutor notes

---
+++ index.js
@@ -0,0 +1,27 @@
+// Fetch weather api data from https://openweathermap.org/api
+//Be able to search for a city on the left side with an input and a search button
+//search button needs a event listener to kick off the API call
+// input for city needs to be the filter
+// also on the left side I need a recent searches buttons
+// I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index -- pulled from the API
+// insomnia to test what keys i get back from the API, make a list of relevant keys 
+// UV index color based color that indicates whether the conditions are favorable, moderate, or severe 
+    // if then statements for coloring 
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+// past searches need to be dynamically created and clickable to show data again
+
+//past searches are saved in localstoage
+
+// moment.js to incorporate current date and next days
+
+// search for a city 
+// 
+// use the geocoding API to input city and return lat, long coordinates
+// pass those values into the API call URL
+// pull value from text input and insert into https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}  
+
+//pull lat and long from above api call and insert them into 
+
+//
